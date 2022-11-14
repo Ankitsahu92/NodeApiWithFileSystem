058f057fb2f8e4ac246b5511ba6ac8130fe1a105
Ankit Sahu <urankitsahu92@gmail.com>
2022-11-14 21:02:12 +0530

    Dev (#1)
    * customer APi
    
    * Added generic API
    
    * removed unused files

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// const path = require('path');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
 const app = express();
 const routePrefix = "/api";
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 const auth = require('./routes/auth');
+const accounts = require('./routes/accounts');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(`${routePrefix}/admin`, adminRoutes);
 app.use(`${routePrefix}/users`, userRoutes);
 app.use(`${routePrefix}/auth`, auth);
-//app.use(shopRoutes);
+app.use(`${routePrefix}/accounts`, accounts);
 
-//app.use(errorController.get404);
+const customer = require('./routes/customer');
+app.use(`${routePrefix}/customer`, customer);
 
 app.listen(5000);
---
+++ controllers/customer.js
@@ -0,0 +1,23 @@
+const customer = require("../models/customer");
+const User = require("../models/user");
+const generic = require("../util/output.service");
+const bcrypt = require('bcryptjs');
+
+
+exports.registration = async (req, res, next) => {
+    const name = req.body.name;
+    const userName = req.body.userName;
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const isActive = true;
+    const userType = req.body.userType;
+    const item = new User(null, name, userName, password, isActive, userType);
+    item.save((resp) => {
+        res.json(resp);
+    });
+};
+
+exports.authenticate = async (req, res, next) => {
+    const body = req.body;
+    User.auth(body, resp => res.json(resp));
+};
